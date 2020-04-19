b6d297a74e8f433bd2ee0827d7ca7a44e2352660
Christoffer G. Thomsen <chris@cgt.name>
2020-04-19 19:28:50 +0200

    Working

--- transactions.rb
+++ transactions.rb
@@ -12,11 +12,11 @@ def assert_throws(exception_class, &block)
     end
 end
 
-class FooError < RuntimeError
+class DerivedFromRuntimeError < RuntimeError
 end
 
 assert_throws(RuntimeError) do
-    raise FooError.new
+    raise DerivedFromRuntimeError.new
 end
 
 class Transaction
