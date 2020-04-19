84c6cab326bf406c1716522a53450c6442a5986f
Christoffer G. Thomsen <chris@cgt.name>
2020-04-19 18:25:18 +0200

    Working

--- transactions.rb
+++ transactions.rb
@@ -3,19 +3,20 @@ def assert(bool)
 end
 
 class Transaction
-    attr_reader :previous, :from, :to
+    attr_reader :previous, :from, :to, :points
 
-    def initialize(previous:, from:, to:)
+    def initialize(previous:, from:, to:, points:)
         @previous = previous
         @from = from
         @to = to
+        @points = points
     end
 end
 
-t = Transaction.new(previous: nil, from: "Alice", to: "Bob")
+t = Transaction.new(previous: nil, from: "Alice", to: "Bob", points: 2)
 assert t.previous == nil
 assert t.from == "Alice"
 assert t.to == "Bob"
 
-t2 = Transaction.new(previous: t, from: "Bob", to: "Alice")
+t2 = Transaction.new(previous: t, from: "Bob", to: "Alice", points: 2)
 assert t2.previous == t
