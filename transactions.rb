a5bfccbf623de3429d93013c1c00c15a5a810463
Christoffer G. Thomsen <chris@cgt.name>
2020-04-19 18:34:20 +0200

    Working

--- transactions.rb
+++ transactions.rb
@@ -40,7 +40,7 @@ assert x.transactions.size == 0
 
 x.send_points(from: "Alice", to: "Bob", points: 2)
 assert x.transactions.size == 1
-assert x.transactions.last
+assert x.transactions.last.from == "Alice"
 
 x.send_points(from: "Bob", to: "Alice", points: 2)
 assert x.transactions.size == 2
